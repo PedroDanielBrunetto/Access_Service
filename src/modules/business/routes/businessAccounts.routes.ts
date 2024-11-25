cff7c3ff9b0a5ce011371219e4fc27a15586615d
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2024-11-25 19:08:50 -0300

    feat: middleware isAuthenticated

--- src/modules/business/routes/businessAccounts.routes.ts
+++ src/modules/business/routes/businessAccounts.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import BusinessAccountsController from "../controllers/BusinessAccountsController";
 import { validateCreateBusinessAccount } from "../validation/CreateAccountBusinessSchema";
 import { validateUpdateBusinessAccount } from "../validation/UpdateAccountBusinessSchema";
+import isAuthenticated from "@/shared/http/middlewares/isAuthenticated";
 
 const businessAccountsRouter = Router();
 const accountController = new BusinessAccountsController();
@@ -9,7 +10,7 @@ const accountController = new BusinessAccountsController();
 businessAccountsRouter.post("/", validateCreateBusinessAccount, (req, res, next) => {
   accountController.create(req, res).catch(next);
 });
-businessAccountsRouter.put("/", validateUpdateBusinessAccount, (req, res, next) => {
+businessAccountsRouter.put("/", validateUpdateBusinessAccount, isAuthenticated, (req, res, next) => {
   accountController.update(req, res).catch(next);
 });
 
